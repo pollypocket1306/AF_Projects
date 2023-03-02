57a1011bf92814c2cfb228646df35a163d77126b
pollypocket1306 <97873736+pollypocket1306@users.noreply.github.com>
2023-03-02 09:37:32 +0200

    commit la final de ora

---
+++ Curs1_P1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Curs1_P1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lstbContent = new System.Windows.Forms.ListBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(121, 281);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(139, 58);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lstbContent
+            // 
+            this.lstbContent.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbContent.FormattingEnabled = true;
+            this.lstbContent.ItemHeight = 25;
+            this.lstbContent.Location = new System.Drawing.Point(12, 12);
+            this.lstbContent.Name = "lstbContent";
+            this.lstbContent.Size = new System.Drawing.Size(357, 154);
+            this.lstbContent.TabIndex = 1;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 182);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(357, 23);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lstbContent);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ListBox lstbContent;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Curs1_P1/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Curs1_P1
+{
+    public partial class Form1 : Form
+    {
+        Random rnd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lstbContent.Items.Clear();
+            rnd = new Random();
+            int[] array = new int[5];
+            int[] frecv = new int[15];
+
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = rnd.Next(2, 15);
+                frecv[array[i]]++;
+                lstbContent.Items.Add(array[i]);
+            }
+
+            string buffer = "";
+            int max = frecv[0];
+            int min = frecv[0];
+            int cont2 = 0;
+
+            for (int i = 0; i < 15; i++)
+            {
+                buffer += frecv[i] + " ";
+
+                if (frecv[i] > max)
+                {
+                    max = frecv[i];
+                }
+
+                if (frecv[i] < min)
+                {
+                    min = frecv[i];
+                }
+
+                if (frecv[i] == 2)
+                {
+                    cont2++;
+                }
+            }
+            lstbContent.Items.Add(buffer);
+
+            if (max == 5)
+            {
+                lblResult.Text = "5";
+            }
+            else if (max == 4)
+            {
+                lblResult.Text = "4";
+            }
+            else if (max == 3 && cont2 == 1)
+            {
+                lblResult.Text = "3 & 2";
+            }
+            else if (max == 3)
+            {
+                lblResult.Text = "3";
+            }
+            else if (cont2 == 2)
+            {
+                lblResult.Text = "2 & 2";
+            }
+            else if (cont2 == 1)
+            {
+                lblResult.Text = "2";
+            }
+            else
+            {
+                lblResult.Text = "No Case";
+            }
+        }
+    }
+}
